3ebd4aa37938c37bc1895f6983fa7a369fbeccb1
Rubén Muñoz <75849510+rbnicodev@users.noreply.github.com>
2024-03-16 13:29:06 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { createGlobalStyle } from "styled-components";
+//TODO: Aprender REACT
 
 const Global = createGlobalStyle`
   body {
